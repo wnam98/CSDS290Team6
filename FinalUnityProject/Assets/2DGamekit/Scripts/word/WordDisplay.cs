a078c9230b90a6e5dd639dfacb2be6a1afd58a77
Ningjia Huang <nxh239@case.edu>
2021-04-27 10:48:35 -0400

    merged two branches.

---
+++ FinalUnityProject/Assets/2DGamekit/Scripts/word/WordDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WordDisplay : MonoBehaviour
+{
+    public Text text;
+    public float fallSpeed = 60.0f;
+    public bool getSpell = true;
+    ParticleSystem particle;
+    void Start(){
+        particle = GetComponent<ParticleSystem>();
+    }
+    public void SetWord(string word)
+    {
+        text.text = word;
+    }
+
+    public void RemoveLetter()
+    {
+        text.text = text.text.Remove(0, 1);
+        text.color = Color.red;
+    }
+
+    public void RemoveWord()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Translate(0.0f, -fallSpeed * Time.deltaTime, 0.0f);
+        float position_y = transform.position.y;
+        if (position_y <= 40)
+        {
+            Destroy(gameObject);
+            getSpell = false;
+            particle.Play();
+        }
+    }
+}
---
+++ FinalUnityProject/Assets/2DGamekit/Scripts/word/WordInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordInput : MonoBehaviour
+{
+    public WordController wordController;
+
+    // Update is called once per frame
+    void Update()
+    {
+       foreach(char letter in Input.inputString)
+       {
+            wordController.TypeLetter(letter);
+       }
+    }
+}
