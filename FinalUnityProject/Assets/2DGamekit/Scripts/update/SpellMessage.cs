c49d066ec4a4177aaaf0e9c0579b8c1046edc947
Xiaoyao <xxn23@case.edu>
2021-05-05 00:14:47 +0800

    stage4 update

---
+++ FinalUnityProject/Assets/2DGamekit/Scripts/update/SpellMessage.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellMessage : MonoBehaviour
+{
+    private int fire_num = 1;
+    private int water_num = 0;
+    private int plant_num = 0;
+
+    private int group;
+    private bool success_added;
+
+    private Text spellAbility;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spellAbility = gameObject.GetComponent<Text>();
+        success_added = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int success_typed = GameObject.Find("WordsController").GetComponent<WordController>().get_successful();
+        if (success_typed == 0)
+        {
+            group = SpellGenerator.get_group();
+            success_added = false;
+        }
+        if (success_typed >= 20 && !success_added)
+        {
+            if (group == 0)
+            {
+                fire_num++;
+                success_added = true;
+            }
+            else if (group == 1)
+            {
+                water_num++;
+                success_added = true;
+            }
+            else
+            {
+                plant_num++;
+                success_added = true;
+            }
+        }
+       
+         
+        spellAbility.text = "Fire: " + fire_num + "\nwater: " + water_num + "\nplant: " + plant_num + "\nTyped: " + success_typed;
+    }
+
+    public int get_fire_num()
+    {
+        return fire_num;
+    }
+
+    public int get_water_num()
+    {
+        return water_num;
+    }
+
+    public int get_plant_num()
+    {
+        return plant_num;
+    }
+
+    public void update_spell_num(int fire, int water, int plant)
+    {
+        fire_num = fire;
+        water_num = water;
+        plant_num = plant;
+    }
+
+}
---
+++ FinalUnityProject/Assets/2DGamekit/Scripts/update/WordDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WordDisplay : MonoBehaviour
+{
+    public Text text;
+    public float fallSpeed = 60.0f;
+    public bool getSpell = true;
+
+    public void SetWord(string word)
+    {
+        text.text = word;
+    }
+
+    public void RemoveLetter()
+    {
+        text.text = text.text.Remove(0, 1);
+        text.color = Color.red;
+    }
+
+    public void RemoveWord()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Translate(0.0f, -fallSpeed * Time.deltaTime, 0.0f);
+        float position_y = transform.position.y;
+        if (position_y <= 130)
+        {
+            Destroy(gameObject);
+            getSpell = false;
+            Debug.Log("game over");
+        }
+    }
+}
